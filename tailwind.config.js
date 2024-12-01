1be15e0477daa0e429df54e025b358de28e556d2
Mateusz Buchajewicz <theherrlupardo@gmail.com>
2024-12-01 11:10:16 +0100

    chore: added missing files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const { join } = require('path');
+const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
