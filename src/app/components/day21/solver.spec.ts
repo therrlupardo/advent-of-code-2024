c7557ce790a2f5f70fcee65491005e5f4feb6465
Mateusz Buchajewicz <theherrlupardo@gmail.com>
2025-01-14 16:21:02 +0100

    feat(day21): solution for day 21 part 2. Optimized solution for part 1

--- src/app/components/day21/solver.spec.ts
+++ src/app/components/day21/solver.spec.ts
@@ -6,86 +6,92 @@ const DAY_21_TEST1_INPUT = `029A
 456A
 379A`;
 describe('Day 21: Keypad Conundrum', () => {
-  it('should parse input', () => {
-    const solver = new Solver();
-    solver.parseInput(DAY_21_TEST1_INPUT);
-    const codes = solver.getCodes();
-    expect(codes.length).toBe(5);
-    expect(codes[0]).toBe('029A');
-    expect(codes[1]).toBe('980A');
-    expect(codes[2]).toBe('179A');
-    expect(codes[3]).toBe('456A');
-    expect(codes[4]).toBe('379A');
+  describe('Parsing input', () => {
+    it('should parse input', () => {
+      const solver = new Solver();
+      solver.parseInput(DAY_21_TEST1_INPUT);
+      const codes = solver.getCodes();
+      expect(codes.length).toBe(5);
+      expect(codes[0]).toBe('029A');
+      expect(codes[1]).toBe('980A');
+      expect(codes[2]).toBe('179A');
+      expect(codes[3]).toBe('456A');
+      expect(codes[4]).toBe('379A');
 
-  });
-  it('should give solution for first step', () => {
-    const solver = new Solver();
-    const solution = solver.solveFirstStep('029A');
-    console.debug(solution);
-    expect(solution).toHaveLength(12)
+    });
   })
 
-  test.each([
-    ['029A', 68],
-    ['980A', 60],
-    ['179A', 68],
-    ['456A', 64],
-    ['379A', 64]
-  ])(`should solve first task for %s`, (input, expected) => {
-    const solver = new Solver();
-    const solution = solver.solveFirstTaskFor(input);
-    expect(solution).toBe(expected);
-  });
-
-  test.each([
-    ['029A', 29],
-    ['980A', 980],
-    ['179A', 179],
-    ['456A', 456],
-    ['379A', 379]
-  ])('should get number from input %s', (input, expected) => {
-    const solver = new Solver();
-    const solution = solver.getNumberFromInput(input);
-    expect(solution).toBe(expected);
+  describe('Finding shortest path for 2 robots', () => {
+    test.each([
+      ['029A', 68],
+      ['980A', 60],
+      ['179A', 68],
+      ['456A', 64],
+      ['379A', 64],
+      ['805A', 72],
+      ['964A', 72],
+      ['459A', 74],
+      ['968A', 70],
+      ['671A', 74]
+    ])(`should solve first task for %s`, (input, expected) => {
+      const solver = new Solver();
+      const calculatedSolution = solver.calculateShortestPathFor(input, 2);
+      expect(calculatedSolution).toBe(expected)
+    });
   });
 
-  it('should solve entire task 1 example', () => {
-    const solver = new Solver();
-    const subtasks = solver.parseInput(DAY_21_TEST1_INPUT);
-    const subtaskSolutions = subtasks.map(subtask => ({
-      subtask,
-      solution: solver.solveFirstTaskFor(subtask),
-      numericValue: solver.getNumberFromInput(subtask)
-    }))
-    console.debug(subtaskSolutions);
-    const solution = subtaskSolutions.reduce((acc, subtask) => acc + subtask.solution * subtask.numericValue, 0);
-    // const solution = subtasks.reduce((acc, subtask) => acc + solver.solveFirstTaskFor(subtask) * solver.getNumberFromInput(subtask), 0);
-    expect(solution).toEqual(126384);
+  describe('Finding solution for examples with 2 robots', () => {
+
+    test.each([
+      ['029A', 29],
+      ['980A', 980],
+      ['179A', 179],
+      ['456A', 456],
+      ['379A', 379]
+    ])('should get number from input %s', (input, expected) => {
+      const solver = new Solver();
+      const solution = solver.getNumberFromInput(input);
+      expect(solution).toBe(expected);
+    });
+
+    it('should solve entire task 1 example', () => {
+      const solver = new Solver();
+      const subtasks = solver.parseInput(DAY_21_TEST1_INPUT);
+      const subtaskSolutions = subtasks.map(subtask => ({
+        subtask,
+        solution: solver.calculateShortestPathFor(subtask, 2),
+        numericValue: solver.getNumberFromInput(subtask)
+      }))
+      console.debug(subtaskSolutions);
+      const solution = subtaskSolutions.reduce((acc, subtask) => acc + subtask.solution * subtask.numericValue, 0);
+      expect(solution).toEqual(126384);
+    })
   })
 
-  it('should check test values', () => {
-    const solver = new Solver();
-    const option = ['<<^^', '^^<<', '<^<^', '^<^<', '<^^<', '^<<^'];
-    const result = option.map(opt => solver.solveSecondStep(opt));
-    console.debug(option.map((opt, i) => (`${opt}: ${result[i]} (${result[i].length})`)));
-    expect(result).toHaveLength(6);
-  })
-
-  it('should find shortest path for nums', () => {
-    const solver = new Solver();
-    expect(solver.findShortestPathForNums('37')).toBe('v<A<AA>^>AAvA^<A>AAvA^A')
-  })
-
-  it('should solve final example for task 1', () => {
-    const solver = new Solver();
-    const solutions = ['805A', '964A', '459A', '968A','671A'].map(subtask => ({
-      subtask,
-      solution: solver.solveFirstTaskFor(subtask),
-      numericValue: solver.getNumberFromInput(subtask)
-    }));
-    console.debug(solutions);
-    const solution = solutions.reduce((acc, subtask) => acc + subtask.solution * subtask.numericValue, 0);
-    console.debug(solution);
-    expect(solution).toEqual(278748);
+  describe('Final examples', () => {
+    it('should solve final example for task 1', () => {
+      const solver = new Solver();
+      const solutions = ['805A', '964A', '459A', '968A','671A'].map(subtask => ({
+        subtask,
+        solution: solver.calculateShortestPathFor(subtask, 2),
+        numericValue: solver.getNumberFromInput(subtask)
+      }));
+      console.debug(solutions);
+      const solution = solutions.reduce((acc, subtask) => acc + subtask.solution * subtask.numericValue, 0);
+      console.debug(solution);
+      expect(solution).toEqual(278748);
+    })
+    it('should solve final example for task 2', () => {
+      const solver = new Solver();
+      const solutions = ['805A', '964A', '459A', '968A','671A'].map(subtask => ({
+        subtask,
+        solution: solver.calculateShortestPathFor(subtask, 25),
+        numericValue: solver.getNumberFromInput(subtask)
+      }));
+      console.debug(solutions);
+      const solution = solutions.reduce((acc, subtask) => acc + subtask.solution * subtask.numericValue, 0);
+      console.debug(solution);
+      expect(solution).toEqual(337744744231414);
+    })
   })
 })
--- src/app/components/day21/solver.ts
+++ src/app/components/day21/solver.ts
@@ -1,5 +1,6 @@
 export class Solver {
   protected codes: string[] = [];
+  private cache: Record<string, number> = {}
 
   // region getters
   public getCodes(): string[] {
@@ -21,158 +22,125 @@ export class Solver {
     return codes;
   }
 
-  public solveFirstStep(input: string): string {
+  public calculateShortestPathFor(input: string, numberOfRobotSteps: number): number {
+    const entryKey = `${input}-${numberOfRobotSteps}`;
+    if (entryKey in this.cache) {
+      // console.debug(`Read ${entryKey} from cache`, this.cache[entryKey]);
+      return this.cache[entryKey];
+    }
     const pairs = [`A${input[0]}`].concat(Array.from(
       { length: input.length - 1 },
       (_, i) => input.slice(i, i + 2)
     ));
-    // console.debug(pairs);
-    return pairs
-      .map(pair => this.shortestPathsOnNumPad[pair as keyof typeof this.shortestPathsOnNumPad])
-      .join('');
-  }
 
-  public solveSecondStep(input: string): string {
-    // console.debug(input);
-    const pairs = [`A${input[0]}`].concat(Array.from(
-      { length: input.length - 1 },
-      (_, i) => input.slice(i, i + 2)
-    ));
-    // console.debug(pairs);
-    const pairPaths = pairs
-      .map(pair => this.shortestPathsOnArrowPad[pair as keyof typeof this.shortestPathsOnArrowPad]);
-    // console.debug(pairs.map((pair, i) => `${pair}:  ${pairPaths[i]}`));
-    return pairPaths.join('');
-  }
-
-  public solveFirstTaskFor(input: string): number {
-    const pairs = [`A${input[0]}`].concat(Array.from(
-      { length: input.length - 1 },
-      (_, i) => input.slice(i, i + 2)
-    ));
-    // console.debug(pairs);
-    const solution = pairs
-      .map(pair => this.findShortestPathForNums(pair))
-      .join('');
-    // console.debug(solution);
-    return solution.length;
+    if (numberOfRobotSteps === 0) {
+      // take first one, as they are all the same length so it has no meaning
+      const result = pairs.map(pair => shortestPaths[pair as keyof typeof shortestPaths][0]).join('').length;
+      if (!(entryKey in this.cache)) {
+        this.cache[entryKey] = result;
+      }
+      return result;
+    }
+    const result = pairs.map(pair => {
+      const options = shortestPaths[pair as keyof typeof shortestPaths];
+      return options.map(option => this.calculateShortestPathFor(option, numberOfRobotSteps - 1))
+        .reduce((acc, val) => Math.min(acc, val), Number.MAX_SAFE_INTEGER);
+    }).reduce((acc, val) => acc + val, 0);
+    if (!(entryKey in this.cache)) {
+      this.cache[entryKey] = result;
+    }
+    return result;
   }
 
   public getNumberFromInput(input: string): number {
     return Number.parseInt(input.replace(/A/g, ''));
   }
 
-  public findShortestPathForNums(input: string): string {
-    const paths = this.shortestPathsOnNumPad[input as keyof typeof this.shortestPathsOnNumPad];
-    const results = paths.map(path => {
-      // console.debug(path);
-      return this.findAllPathsForArrows(path);
-    }).flat();
-    // console.debug(results);
-    const shortest = results.reduce((acc, path) => {
-      return acc.length < path.length ? acc : path;
-    }, results[0]);
+  // public createShortestPathFor(input: string, totalLoops: number): string {
+  //   const path = shortestPaths[input as keyof typeof shortestPaths];
+  //   return this.findAllPathsForArrows(path, 1, totalLoops);
+  // }
 
-    return shortest;
-  }
+  // private findAllPathsForArrows(input: string, loopIndex: number, totalLoops: number): string {
+  //   const pairs = [`A${input[0]}`].concat(Array.from(
+  //     { length: input.length - 1 },
+  //     (_, i) => input.slice(i, i + 2)
+  //   ));
+  //   const maps = pairs.map(pair => {
+  //     const path = shortestPaths[pair as keyof typeof shortestPaths];
+  //     return loopIndex !== totalLoops ? this.findAllPathsForArrows(path, loopIndex + 1, totalLoops) : this.findAllPathsForArrowsFinalLoop(path)
+  //   });
+  //   return maps.join('');
+  //
+  // }
 
-  public findAllPathsForArrows(input: string): string[] {
-    const pairs = [`A${input[0]}`].concat(Array.from(
+  private findAllPathsForArrowsFinalLoop(input: string): string {
+    const pathPairs = [`A${input[0]}`].concat(Array.from(
       { length: input.length - 1 },
       (_, i) => input.slice(i, i + 2)
     ));
-    // console.debug(pairs);
-    const maps = pairs.map(pair => this.shortestPathsOnArrowPad[pair as keyof typeof this.shortestPathsOnArrowPad]
-      .map(path => {
-        // console.debug(`Checking pair ${pair} path ${path}`);
-        const pathPairs = [`A${path[0]}`].concat(Array.from(
-          { length: path.length - 1 },
-          (_, i) => path.slice(i, i + 2)
-        ));
-        const subpairs = pathPairs.map(subpair => this.shortestPathsOnArrowPad[subpair as keyof typeof this.shortestPathsOnArrowPad]);
-        return this.generateCombinations(subpairs).map(comb => comb.join(''));
-      }).flat());
-    // maps.forEach( console.debug);
-    return this.generateCombinations(maps).map(x => x.join(''));
-
+    const subpairs = pathPairs.map(subpair => shortestPaths[subpair as keyof typeof shortestPaths]);
+    return subpairs.join('');
   }
+}
 
-  private generateCombinations(lists: string[][]): string[][] {
-    // Handle empty input or single empty list
-    if (!lists.length) return [];
-    if (lists.length === 1) return lists[0].map(item => [item]);
+const shortestPaths = {'0A': ['>A'],
+  '02': ['^A'],
+  '05': ['^^A'],
+  'A0': ['<A'],
+  'A1': ['^<<A', '<^<A'],
+  'A3': ['^A'],
+  'A4': ['^^<<A', '^<^<A', '^<<^A', '<^^<A', '<^<^A'],
+  'A6': ['^^A'],
+  'A8': ['^^^<A', '^^<^A', '^<^^A', '<^^^A'],
+  'A9': ['^^^A'],
+  '1A': ['>>vA', '>v>A'],
+  '17': ['^^A'],
+  '29': ['^^>A'],
+  '37': ['<<^^A', '<^<^A', '<^^<A', '^<<^A', '^<^<A', '^^<<A'],
+  '4A': ['>>vvA', '>v>vA', '>vv>A', 'v>v>A', 'v>>vA'],
+  '45': ['>A'],
+  '5A': ['>vvA', 'v>vA', 'vv>A'],
+  '56': ['>A'],
+  '59': ['^>A', '>^A'],
+  '6A': ['vvA'],
+  '64': ['<<A'],
+  '67': ['^<<A', '<^<A', '<<^A'],
+  '68': ['^<A', '<^A'],
+  '71': ['vvA'],
+  '79': ['>>A'],
+  '80': ['vvvA'],
+  '8A': ['>vvvA', 'v>vvA', 'vv>vA', 'vvv>A'],
+  '9A': ['vvvA'],
+  '96': ['vA'],
+  '98': ['<A'],
+  'AA': ['A'],
+  'A>': ['vA'],
+  'A<': ['v<<A', '<v<A'],
+  'A^': ['<A'],
+  'Av': ['<vA', 'v<A'],
 
-    // Take the first list and generate combinations with the rest
-    const [first, ...rest] = lists;
-    const combinationsOfRest = this.generateCombinations(rest);
+  '>A': ['^A'],
+  '>>': ['A'],
+  '>^': ['<^A', '^<A'],
+  '><': ['<<A'],
+  '>v': ['<A'],
 
-    // Combine each item from the first list with all combinations of the rest
-    return first.flatMap(item =>
-      combinationsOfRest.map(combination => [item, ...combination])
-    );
-  }
+  '<A': ['>>^A', '>^>A'],
+  '<>': ['>>A'],
+  '<^': ['>^A'],
+  '<<': ['A'],
+  '<v': ['>A'],
 
-  protected shortestPathsOnNumPad = {
-    '0A': ['>A'],
-    '02': ['^A'],
-    '05': ['^^A'],
-    'A0': ['<A'],
-    'A1': ['^<<A', '<^<A'],
-    'A3': ['^A'],
-    'A4': ['^^<<A', '^<^<A', '^<<^A', '<^^<A', '<^<^A'],
-    'A6': ['^^A'],
-    'A8': ['^^^<A', '^^<^A', '^<^^A', '<^^^A'],
-    'A9': ['^^^A'],
-    '1A': ['>>vA', '>v>A'],
-    '17': ['^^A'],
-    '29': ['^^>A'],
-    '37': ['<<^^A', '<^<^A', '<^^<A', '^<<^A', '^<^<A', '^^<<A'],
-    '4A': ['>>vvA', '>v>vA', '>vv>A', 'v>v>A', 'v>>vA'],
-    '45': ['>A'],
-    '5A': ['>vvA', 'v>vA', 'vv>A'],
-    '56': ['>A'],
-    '59': ['^>A', '>^A'],
-    '6A': ['vvA'],
-    '64': ['<<A'],
-    '67': ['^<<A', '<^<A', '<<^A'],
-    '68': ['^<A', '<^A'],
-    '71': ['vvA'],
-    '79': ['>>A'],
-    '80': ['vvvA'],
-    '8A': ['>vvvA', 'v>vvA', 'vv>vA', 'vvv>A'],
-    '9A': ['vvvA'],
-    '96': ['vA'],
-    '98': ['<A'],
-  };
-  protected shortestPathsOnArrowPad = {
-    'AA': ['A'],
-    'A>': ['vA'],
-    'A<': ['v<<A', '<v<A'],
-    'A^': ['<A'],
-    'Av': ['<vA', 'v<A'],
+  '^A': ['>A'],
+  '^>': ['>vA', 'v>A'],
+  '^<': ['v<A'],
+  '^v': ['vA'],
+  '^^': ['A'],
 
-    '>A': ['^A'],
-    '>>': ['A'],
-    '>^': ['<^A', '^<A'],
-    '><': ['<<A'],
-    '>v': ['<A'],
-
-    '<A': ['>>^A', '>^>A'],
-    '<>': ['>>A'],
-    '<^': ['>^A'],
-    '<<': ['A'],
-    '<v': ['>A'],
-
-    '^A': ['>A'],
-    '^>': ['>vA', 'v>A'],
-    '^<': ['v<A'],
-    '^v': ['vA'],
-    '^^': ['A'],
-
-    'vA': ['^>A', '<^A'],
-    'v>': ['>A'],
-    'v<': ['<A'],
-    'v^': ['^A'],
-    'vv': ['A'],
-  };
+  'vA': ['^>A', '<^A'],
+  'v>': ['>A'],
+  'v<': ['<A'],
+  'v^': ['^A'],
+  'vv': ['A'],
 };
